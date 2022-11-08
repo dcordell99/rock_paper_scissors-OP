1d9cf209d731f05db0a2770bef94213b338db92c
dcordell99 <davidcordell@hotmail.com>
2022-11-08 11:39:49 -0500

    Add style.css and add UI to game

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+const beginGame = () => {
+	const playerChoices = document.querySelectorAll('.btn-choice'); 
+	let round = 0;
+	let playerWins = 0
+	let computerWins = 0
+	let ties = 0;
+
+	playerChoices.forEach(choice => {
+		choice.addEventListener('click', (e) => { 
+			if (round > 4) {
+				if (confirm('Game is over, restart')) {
+					location.reload()
+				} else {
+					return;
+				}
+			}
+			round++;
+			let playerGuess = e.target.getAttribute('id');
+			let computerGuess = computerPlay();
+			let roundWinner = playRound(playerGuess, computerGuess);
+
+			if (roundWinner === 'tie') {
+				ties++;
+			} else if (roundWinner && roundWinner !== 'tie') {
+				playerWins++;
+				document.querySelector('.player-wins').textContent = playerWins.toString();
+			} else if (!roundWinner) {
+				computerWins++;
+				document.querySelector('.computer-wins').textContent = computerWins.toString();
+			}
+
+			if (round == 5 || playerWins == 3 || computerWins == 3) {
+				let showWinner = document.querySelector('#game_won');
+
+				playerChoices.forEach(button => { 
+					button.classList.add('btn-game_over');
+				});
+
+				if (playerWins > computerWins) {
+					showWinner.textContent = `You won the game!!!`;
+				} else if (computerWins > playerWins) {
+					showWinner.textContent = `You lost! Better luck next time!`;
+				} else {
+					showWinner.textContent = `Close call, but it looks like it was a tie this time!`;
+				}
+			}
+		})
+	})
+}
+
 const computerPlay = () => {
 	let guess;
 	let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -12,24 +62,9 @@ const computerPlay = () => {
 	return guess;
 };
 
-const playerGuess = () => {
-	let guess;
-	let isWrongAnswer = true;
-	while (isWrongAnswer) {
-		guess = prompt('Rock, paper or scissors?').toLowerCase();
-		if (guess === 'rock' || guess === 'paper' || guess === 'scissors') {
-			isWrongAnswer = false;
-		} else {
-			alert(
-				'Incorrect Response. Correct guesses are "rock", "paper" or "scissors".'
-			);
-		}
-	}
-	return guess;
-};
-
 function playRound(playerSelection, computerSelection) {
-	let gameResult;
+	let gameResult = false;
+	let roundWinner = document.querySelector('#round_won')
 	playerSelection =
 		playerSelection.substr(0, 1).toUpperCase() + playerSelection.substr(1);
 	computerSelection =
@@ -38,45 +73,23 @@ function playRound(playerSelection, computerSelection) {
 	(playerSelection === 'Rock' && computerSelection === 'Scissors') ||
 	(playerSelection === 'Scissors' && computerSelection === 'Paper') ||
 	(playerSelection === 'Paper' && computerSelection === 'Rock')
-		? (console.log(`You win! ${playerSelection} beats ${computerSelection}.`),
-			(gameResult = true))
+		? (roundWinner.innerHTML = `You win! <span style="text-decoration: underline;">${playerSelection}</span> beats ${computerSelection}.`,
+			(gameResult = true),
+			(roundWinner.style.color = 'green'))
 		: (playerSelection === 'Rock' && computerSelection === 'Paper') ||
 			(playerSelection === 'Scissors' && computerSelection === 'Rock') ||
 			(playerSelection === 'Paper' && computerSelection === 'Scissors')
-			? (console.log(
-					`You lose! ${computerSelection} beats ${playerSelection}.`
-				),
-				(gameResult = false))
-			: (console.log(
-					`Tie! ${playerSelection} and ${computerSelection} are the same!`
-				),
-				(gameResult = 'tie'));
+			? (roundWinner.innerHTML = `You lose! <span style="text-decoration: underline;">${computerSelection}</span> beats ${playerSelection}.`,
+				(roundWinner.style.color = 'red'))
+			: (roundWinner.innerHTML = 
+					`Tie! ${playerSelection} and ${computerSelection} are the same!`,
+					(roundWinner.style.color = 'black'),
+					(gameResult = 'tie'))
 
 	return gameResult;
 }
 
-function game() {
-	let playerWins = 0;
-	let computerWins = 0;
-	let ties = 0;
+let newGameBtn = document.querySelector('.btn-new_game');
+newGameBtn.addEventListener('click', beginGame);
 
-	for (let i = 0; i < 5; i++) {
-		let round = playRound(playerGuess(), computerPlay());
-		if (round === 'tie') {
-			ties++;
-		} else if (round && round !== 'tie') {
-			playerWins++;
-		} else if (!round) {
-			computerWins++;
-		}
-	}
-
-	let message = `You won ${playerWins} rounds.\nThe computer won ${computerWins} rounds.\nThere were ${ties} ties.\n`;
-	if (playerWins > computerWins) {
-		return message + `You won the game!!!`;
-	} else if (computerWins > playerWins) {
-		return message + `You lost! Better luck next time!`;
-	} else {
-		return message + `Close call, but it looks like it was a tie this time!`;
-	}
-}
+beginGame();
